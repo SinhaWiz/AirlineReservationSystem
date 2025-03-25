1976d20617052aa5906b7aa5359f3a394eb7eb51
SinhaWiz <sinhartx2070@gmail.com>
2025-03-25 09:58:02 +0600

    replaced nested if's with (AND)

--- src/RolesAndPermissions.java
+++ src/RolesAndPermissions.java
@@ -10,11 +10,9 @@ public class RolesAndPermissions extends User {
     public int isPrivilegedUserOrNot(String username, String password) {
         int isFound = -1;
         for (int i = 0; i < adminUserNameAndPassword.length; i++) {
-            if (username.equals(adminUserNameAndPassword[i][0])) {
-                if (password.equals(adminUserNameAndPassword[i][1])) {
+            if (username.equals(adminUserNameAndPassword[i][0]) && password.equals(adminUserNameAndPassword[i][1])) {
                     isFound = i;
                     break;
-                }
             }
         }
         return isFound;
